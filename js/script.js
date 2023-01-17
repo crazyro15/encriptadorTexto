96f33cdc61c49453b51438f2a55930edb62ccd9b
crazyro15 <crazyro15@hotmail.com>
2023-01-17 10:32:05 -0300

    vista escritoria lista

--- js/script.js
+++ js/script.js
@@ -1,42 +1,77 @@
-let ingreseTexto = document.getElementById("#ingreseTexto");
+let ingreseTexto = document.getElementById("ingreseTexto");
 let btnEncriptar = document.querySelector("#encriptar");
-let btnDesencriptar = document.getElementById("#desencriptar");
+let btnDesencriptar = document.getElementById("desencriptar");
 let resultado = document.querySelector("#resultado");
-let copiar = document.querySelector("#copiar");
+let copiando = document.querySelector("#copiar");
 let imgCripta = document.querySelector(".imagenCripta");
 let parrafoUno = document.querySelector(".parrafoUno");
 let parrafoDos = document.querySelector(".parrafoDos");
 
+//(querySelector se indica . o # | getElementById no)
 
-function mostrar(){
-    resultado.style.visibility = "visible";
-    imgCripta.style.visibility = "hidden";
-    parrafoUno.style.visibility = "hidden";
-    parrafoDos.style.visibility = "hidden";
-    copiar.style.visibility = "visible";
+function encriptar() {
+    if (ingreseTexto.value == "") {
+
+        resultado.style.visibility = "hidden";
+        imgCripta.style.visibility = "visible";
+        parrafoUno.style.visibility = "visible";
+        parrafoUno.textContent = "Debes indicarme que texto encriptar!";
+        parrafoDos.style.visibility = "visible";
+        copiando.style.visibility = "hidden";
+
+    } else {
+
+        resultado.style.visibility = "visible";
+        imgCripta.style.visibility = "hidden";
+        parrafoUno.style.visibility = "hidden";
+        parrafoDos.style.visibility = "hidden";
+        copiando.style.visibility = "visible";
+        let texto = ingreseTexto.value;
+        let encriptado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+        resultado.innerHTML = encriptado;
+    };
+};
+
+btnEncriptar.addEventListener("click", encriptar);
+
+function desencriptar(){
+    let texto = ingreseTexto.value;
+    let encriptado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    resultado.innerHTML = encriptado;
 }
 
-btnEncriptar.addEventListener("click", mostrar);
-// function encriptar(letras){
-//     switch(letras) {
-//         case 'a':
-//           return 'ia';
-//         case 'e':
-//           return 'enter';
-//         case 'i':
-//           return 'imes';
-//         case 'o':
-//           return 'ober';
-//         case 'u':
-//           return 'ufat';
-//         default:
-//           return letras;
-//       }
-// }
-// function mostrar(){
-//     let resultado = document.querySelector("#testAreaRespuesta");
-//     resultado.style.backgroundColor = red;
-    
-// }
+btnDesencriptar.addEventListener("click", desencriptar);
 
-//btnEncriptar.addEventListener('click',mostrar,true);
+/*function copiar(){
+    navigator.clipboard.readText().then((resultado) => document.querySelector("#resultado").innerHTML = resultado);
+}
+
+copiando.addEventListener("click", copiar);
+*/
+
+/*function copiar(txt){
+    resultado.value = txt;
+    resultado.setAttribute("readonly", "");
+    resultado.select();
+    document.execCommand("copy");
+    document.body.removeChild(resultado);
+}
+
+copiando.addEventListener("click", copiar);
+*/
+
+function copiar(txt){
+    let contenido = document.getElementById('resultado').textContent;
+    let input = document.createElement('input');
+    input.value = contenido;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    input.remove();
+    alert("Texto copiado al portapapeles!");
+}
+
+copiando.addEventListener("click", copiar);
+
+
+
